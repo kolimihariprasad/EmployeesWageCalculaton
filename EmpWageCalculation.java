fba9bac146b4b26be34c4a66c22d5a4661f7296f
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-12 01:14:28 +0530

    Added: Calculating Wages For Month

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -3,25 +3,34 @@ class Employe {
 	final int IS_FULL_TIME=2;
 	final int IS_PART_TIME=1;
 	final int EMP_RATE_PER_HR=20;
+	final int TOTAL_WORKING_DAYS=20;
+	final int TOTAL_WORKING_HOURS=100;
 	//Variables
 	int empHrs=0;
 	int totalSalary=0;
+	int numWorkingDays=1;
+	int totalHrs=0;
 	int EmployeeCheck() {
-		int empCheck=(int)(Math.random() * 10) % 3;
-		switch (empCheck) {
-			case 2:
-				return 8;
-			case 1:
-				return 4;
-			default:
-				return 0;
-		}
+			int empCheck=(int)(Math.random() * 10) % 3;
+			switch (empCheck) {
+				case 2:
+					return 8;
+				case 1:
+					return 4;
+				default:
+					return 0;
+			}
 	}
 	void WageCalculation() {
-		empHrs=EmployeeCheck();
-		totalSalary=empHrs*EMP_RATE_PER_HR;
-		System.out.println(totalSalary);
-
+		while(numWorkingDays<=TOTAL_WORKING_DAYS) {
+			empHrs=EmployeeCheck();
+			if(totalHrs+empHrs<=100) {
+				totalHrs+=empHrs;
+			totalSalary=totalHrs*EMP_RATE_PER_HR;
+			numWorkingDays++;
+			System.out.println(totalSalary);
+			}
+		}
 	}
 	Employe() {
 		WageCalculation();
@@ -30,5 +39,6 @@ class Employe {
 class EmpWageCalculation {
 	public static void main(String arrgs[]) {
 		Employe empOne = new Employe();
+
 	}
 }
