f86b9857869d201a06c9ca19bfe5eac2b21aa4fe
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-11 20:47:06 +0530

    Added: Calculating Daily Employee Wage

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -1,16 +1,29 @@
 class Employe {
+	//Constants
 	final int IS_FULL_TIME=1;
-	void EmployeeCheck() {
+	final int EMP_RATE_PER_HR=20;
+	//Variables
+	int empHrs=0;
+	int totalSalary=0;
+	int EmployeeCheck() {
 		int empCheck=(int)(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Present");
+			return 8;
 		else
-			System.out.println("Absent");
+			return 0;
+	}
+	void WageCalculation() {
+		empHrs=EmployeeCheck();
+		totalSalary=empHrs*EMP_RATE_PER_HR;
+		System.out.println(totalSalary);
+
+	}
+	Employe() {
+		WageCalculation();
 	}
 }
 class EmpWageCalculation {
 	public static void main(String arrgs[]) {
 		Employe empOne = new Employe();
-		empOne.EmployeeCheck();
 	}
 }
