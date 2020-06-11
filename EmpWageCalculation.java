21dd1af0ca0bd4df97cfb66c03662983a50d1769
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-11 21:01:46 +0530

    Refactor: Added Parttime Wage To Condition

--- EmpWageCalculation.java
+++ EmpWageCalculation.java
@@ -1,14 +1,18 @@
 class Employe {
 	//Constants
-	final int IS_FULL_TIME=1;
+	final int IS_FULL_TIME=2;
+	final int IS_PART_TIME=1;
 	final int EMP_RATE_PER_HR=20;
 	//Variables
 	int empHrs=0;
 	int totalSalary=0;
 	int EmployeeCheck() {
-		int empCheck=(int)(Math.random() * 10) % 2;
+		int empCheck=(int)(Math.random() * 10) % 3;
+		 System.out.println(empCheck);
 		if (empCheck == IS_FULL_TIME)
 			return 8;
+		else if(empCheck == IS_PART_TIME)
+			return 4;
 		else
 			return 0;
 	}
